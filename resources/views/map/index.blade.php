55dc00fcb27290cc4d5a3f03e9bd8022024040ac
Victor L <contact@zethr.us>
2026-04-09 15:55:52 -0600

    Fix indentation and formatting in map view scripts section

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -27,8 +27,8 @@
             <div id="map"></div>
         </div>
     </div>
-</x-app-layout>
-@section('scripts')
+
+    @section('scripts')
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
             integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js"></script>
@@ -105,4 +105,5 @@
             }
         });
     </script>
-@endsection
+    @endsection
+</x-app-layout>
